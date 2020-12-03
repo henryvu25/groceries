eff99d8be7b1f8b4f482b438252a216a092c2a07
Henry Vu <henryvu25@gmail.com>
2020-12-02 16:30:18 -0800

    initial commit for groceries app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { GroceriesServiceService } from './groceries-service.service'
+import { InputDialogServiceService } from './input-dialog-service.service'
+
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SocialSharing,
+    GroceriesServiceService,
+    InputDialogServiceService
   ],
   bootstrap: [AppComponent]
 })
